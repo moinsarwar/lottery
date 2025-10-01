819504fe8f7711cd4830bec1ac46e9caa02a2f8b
arslan-ecnet <arslan@ecnetsolutions.net>
2025-10-02 02:33:04 +0500

    changes for security

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,7 +12,7 @@
 <body>
 @include('layouts.home-header')
 @yield('content')
-<script src="js/home.js"></script>
+<script src="{{ asset('js/home.js') }}?v={{ time() }}"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 @yield('script')
